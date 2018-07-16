0353776b5b7857aa467dda712473f69f97aae3f8
nozomi0966 <nozomotoitech@gmail.com>
2018-07-16 16:23:44 +0900

    インデックスが構築できるようになった〜

--- indexmaker/indexmaker.go
+++ indexmaker/indexmaker.go
@@ -4,10 +4,39 @@ import (
 	"strings"
 
 	"github.com/aizu-go-kapro/web-int-searcher/crawler/page"
+	"github.com/aizu-go-kapro/web-int-searcher/mongo"
 	mecab "github.com/bluele/mecab-golang"
 )
 
 func MakeIndex(pages []page.Page) error {
+	for _, page := range pages {
+		page_words, err := ParseText(page.Text)
+		if err != nil {
+			return err
+		}
+		for _, page_word := range page_words {
+			index, err := mongo.GetIndex(page_word)
+			if err != nil {
+				index.Word = page_word
+				index.PageIDs = append(index.PageIDs, page.Id)
+				err = mongo.SaveIndex(index)
+				if err != nil {
+					return err
+				}
+			} else {
+				index.PageIDs = append(index.PageIDs, page.Id)
+				// TODO page.IDを降るようにする
+				err = mongo.UpdateIdex(index)
+				if err != nil {
+					return err
+				}
+			}
+
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
--- indexmaker/indexmaker_test.go
+++ indexmaker/indexmaker_test.go
@@ -3,6 +3,8 @@ package indexmaker
 import (
 	"reflect"
 	"testing"
+
+	"github.com/aizu-go-kapro/web-int-searcher/crawler/page"
 )
 
 func TestParseText(t *testing.T) {
@@ -35,6 +37,57 @@ func TestParseText(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseText() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMakeIndex(t *testing.T) {
+	type args struct {
+		pages []page.Page
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "sample",
+			args: args{
+				[]page.Page{
+					{
+						"hoge",
+						"example.html",
+						"noah",
+						"noahは異常者",
+						[]string{},
+						[]string{},
+					},
+					{
+						"fuga",
+						"hato.html",
+						"hato",
+						"noahは異常者です。なぜなら異常行動をするからです。",
+						[]string{},
+						[]string{},
+					},
+					{
+						"piyo",
+						"hato.html",
+						"hato",
+						"はとバスと八坂さんのドキドキデート",
+						[]string{},
+						[]string{},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MakeIndex(tt.args.pages); (err != nil) != tt.wantErr {
+				t.Errorf("MakeIndex() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
